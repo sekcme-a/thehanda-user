dfca6c418aa8836064ebfcb9b414bfa82a3fa759
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2023-02-22 04:16:08 +0900

    adsf

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import useData from "context/data"
+
 const Home = () => {
+  const {user, userData} = useData()
+
   return(
     <>
+    <p>{user.uid}</p>
+    <p>{userData.displayName}</p>
     </>
   )
 }
