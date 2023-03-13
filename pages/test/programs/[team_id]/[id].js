14c01aff1e7ef186efc55df5a74bfe1a5f20d7f8
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2023-03-14 01:10:21 +0900

    asdf

--- pages/test/programs/[team_id]/[id].js
+++ pages/test/programs/[team_id]/[id].js
@@ -22,51 +22,17 @@ const Survey = () => {
       if(result.exists){
         setData(result.data())
 
-        if(user){
-          db.collection("user").doc(user.uid).collection("history").doc("programs").get().then((doc)=>{
-            if(doc.exists && doc.data().data){
-              //array of object 의 id키와 id가 일치한게 있는지 확인 후 true 반환(history 에 있는지 확인)
-              if(doc.data().data.some(obj => obj.id == id)){
-                alert("이미 신청한 프로그램입니다.")
-                router.push("/")
-              }
-            }
-          })
-        } else{
-          alert("로그인 후 이용해주세요.")
-          router.push("/")
-        }
-    
-        if(result.data().hasLimit){
-          db.collection("team_admin").doc(team_id).collection("result").doc(id).collection("users").get().then((query) => {
-            if(parseInt(result.data().limit)<=query.docs.length)
-              alert("선착순 마감되었습니다.")
-              router.push("/")
-          })
-        }
-    
-        if(result.data().deadline?.toDate()<=new Date()){
-          alert("신청 마감되었습니다.")
-          router.push("/")
-        }
-
 
         setIsLoading(false)
 
 
       } else{
-        alert("존재하지 않거나 삭제된 게시물입니다.")
-        router.push("/")
+        alert("최초 저장 후 미리보기가 가능합니다.")
       }
 
       
     }
-    if(localStorage.getItem("selectedTeamId")===null){
-      localStorage.setItem("selectedTeamId", team_id)
-      db.collection("team").doc(team_id).get().then((doc) => {
-        localStorage.setItem("selectedTeamName", doc.data().teamName)
-      })
-    }
+
     fetchData()
   }, [])
 
